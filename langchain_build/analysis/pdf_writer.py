cafe8c07edbbf9048e885394e4611201bae0bee8
Bijaya Thebe <82489296+BijayaThebe@users.noreply.github.com>
2025-09-15 12:34:54 +0545

    Delete langchain_build directory
    Delete langchain

--- langchain_build/analysis/pdf_writer.py
+++
@@ -1,9 +0,0 @@
-from reportlab.platypus import SimpleDocTemplate, Paragraph
-from reportlab.lib.styles import getSampleStyleSheet
-
-def write_pdf(text, output_file="result.pdf"):
-    doc = SimpleDocTemplate(output_file)
-    styles = getSampleStyleSheet()
-    content = [Paragraph(text, styles["Normal"])]
-    doc.build(content)
-    return output_file
